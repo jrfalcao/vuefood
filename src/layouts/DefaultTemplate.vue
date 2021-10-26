eb1e49996f16f85b6b28a245f81aff4594426783
Junior Falcão <jcjunior@dermage.com.br>
2021-10-26 16:14:37 -0300

    Primeiras Configuracoes

---
+++ src/layouts/DefaultTemplate.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Template</div>
+</template>
+
+<script>
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {};
+</script>
+
+<style scoped>
+@import url("../assets/css/vuefood.css");
+@import url("../assets/css/reset.css");
+</style>
